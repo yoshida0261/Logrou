c37abf1f132fa972ea1b69448dfd947805a2cd2d
yoshida0261 <yoshida261@gmail.com>
2020-12-13 01:51:44 +0900

    Initial commit

---
+++ app/src/main/java/com/stah/logrou/ui/add/AddActivity.kt
@@ -0,0 +1,12 @@
+package com.stah.logrou.ui.add
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.stah.logrou.R
+
+class AddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+    }
+}
---
+++ app/src/main/java/com/stah/logrou/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.stah.logrou.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.stah.logrou.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
